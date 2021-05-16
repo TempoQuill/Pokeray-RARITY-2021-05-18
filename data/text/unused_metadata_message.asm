41fce11922d0b1337783280d70915890de0b4a8c
TempoQuill <jicrapo@gmail.com>
2021-05-16 19:50:24 -0400

    Delete assumed info

--- data/text/unused_metadata_message.asm
+++ data/text/unused_metadata_message.asm
@@ -20,21 +20,6 @@ ENDC
 IF DEF (_SHADE)
 	db   "  #MON SHADE"
 ENDC
-	next "Believe it or not,"
-	next "there are copiers"
-	next "that'll sacrifice"
-	next "metadata preser-"
-	next "vation to stream-"
-	next "line the work each"
-	next "copier has to do.@"
-
-UnusedMetadataCorruptionText2:
-IF DEF (_RAY)
-	db   "   #MON RAY"
-ENDC
-IF DEF (_SHADE)
-	db   "  #MON SHADE"
-ENDC
 	next "Please return this"
 	next "copy to the vendor"
 	next "you've obtained it"
